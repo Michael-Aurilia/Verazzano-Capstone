87a5e843cf9eca26151d2114dd50f5a62a03d6b8
Michael Aurilia <135925083+Michael-Aurilia@users.noreply.github.com>
2023-12-28 23:49:55 -0500

    Update Potion.java

--- Potion.java
+++ Potion.java
@@ -7,25 +7,16 @@ public class Potion implements Item{
 		myPokemon.getName();
 	}
 	@Override
-	public void useItem(Pokemon myPokemon, int maxHP, int count) {
-		//If the program ran the item function before it won't work again.
-		if (count == 1) {
-			System.out.println("You have no items left!");
+	public void useItem(Pokemon myPokemon, int maxHP) {
+		System.out.println(myPokemon.getName() + " was given a  Max Potion!");
+		//If the users Pokemon HP is already full, the potion will be wasted and have no effect.
+		if (myPokemon.getHitPoints() == maxHP) {
+			System.out.println("But their HP was already full! It has no effect!");
 		}
+			
 		else {
-			System.out.println(myPokemon.getName() + " was given a potion!");
-			//If the users Pokemon HP is already full, the potion will be wasted and have no effect.
-			if (myPokemon.getHitPoints() == maxHP) {
-				System.out.println("But their HP was already full! It has no effect!");
-			}
-			//If the Pokemon hasn't lost more than 50HP, it is healed back to max and the user is told how much health it healed as opposed to 50.
-			else  {
-				System.out.println(myPokemon.getName() + " was healed for " + (maxHP - myPokemon.getHitPoints()) + " health!");
-				myPokemon.setHitPoints(maxHP);
-			}
+			System.out.println(myPokemon.getName() + " was healed back to full health!");
+			myPokemon.setHitPoints(maxHP);
 		}
-		
-		
-	}
-	
+	}	
 }
