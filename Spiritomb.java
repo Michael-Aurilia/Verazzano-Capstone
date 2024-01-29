999790a8e25e6b30b4817e2e80940bd17c129ce3
Michael Aurilia <135925083+Michael-Aurilia@users.noreply.github.com>
2024-01-28 20:42:14 -0500

    Update Spiritomb.java

--- Spiritomb.java
+++ Spiritomb.java
@@ -125,6 +125,59 @@ public class Spiritomb extends Pokemon{
 					System.out.println(other.getName() + " takes " + damageDealt + " damage!");
 				}
 	}
+	
+	public List<Integer> getDamages(Pokemon myPokemon, Pokemon other){
+		String battleCry8, PokemonInfo8;
+		List<String> moves8 = new ArrayList<>();
+		List<String> attackTypes8 = new ArrayList<>();
+		List<Integer> basePowers8 = new ArrayList<>();
+		List<String> moveDescriptions8 = new ArrayList<>();
+	
+		//Creates attacks for this Pokemon to use.
+				moves8.add("Shadow Ball");
+				attackTypes8.add("Ghost");
+				basePowers8.add(80);
+				moveDescriptions8.add("Ghost Type, Special: Spiritomb hurls a shadowy blob at the opposing Pokemon!");
+				Attack ShadowBall = new Attack(moveDescriptions8.get(0), 1, 15, attackTypes8.get(0), basePowers8.get(0), "Special");
+				
+				moves8.add("Dark Pulse");
+				attackTypes8.add("Dark");
+				basePowers8.add(80);
+				moveDescriptions8.add("Dark Type, Special: Spiritomb releases a horrible aura imbued with dark thoughts!");
+				Attack DarkPulse = new Attack(moveDescriptions8.get(1), 1, 15, attackTypes8.get(1), basePowers8.get(1), "Special");
+				
+				//Will become Will-o-wisp
+				moves8.add("Icy Wind");
+				attackTypes8.add("Ice");
+				basePowers8.add(55);
+				moveDescriptions8.add("Ice Type, Special: Spiritomb attacks with a gust of chilled air!");
+				Attack IcyWind = new Attack(moveDescriptions8.get(2), 1, 15, attackTypes8.get(2), basePowers8.get(2), "Special");
+				
+				moves8.add("Hex");
+				attackTypes8.add("Ghost");
+				basePowers8.add(65);
+				moveDescriptions8.add("Ghost type, Special: Spiritomb attacks with relentless intensity!");
+				Attack Hex = new Attack(moveDescriptions8.get(3), 1, 10, attackTypes8.get(3), basePowers8.get(3), "Special");
+				
+				PokemonInfo8 = "Spiritomb was formed by 108 spirits. It is bound to a fissure in an Odd Keystone.";
+				battleCry8 = "OOOOOO!";
+				
+				//HP, Type1, Type2, moves, battlecry, atk, def, spAtk, spDef, spe, Info
+				Spiritomb Tomb = new Spiritomb(157, 157, "Healthy", "Spiritomb", "Ghost", "Dark", moves8, battleCry8, 158, 176, 158, 176, 95, PokemonInfo8);
+				
+				int damageDealt1 = ShadowBall.getDamage(ShadowBall, Tomb, other);
+				int damageDealt2 = DarkPulse.getDamage(DarkPulse, Tomb, other);
+				int damageDealt3 = IcyWind.getDamage(IcyWind, Tomb, other);
+				int damageDealt4 = Hex.getDamage(Hex, Tomb, other);
+				
+				List<Integer> damageList = new ArrayList<>();
+				damageList.add(damageDealt1);
+				damageList.add(damageDealt2);
+				damageList.add(damageDealt3);
+				damageList.add(damageDealt4);
+				
+				return damageList;
+	}
 
 	@Override
 	public int compareTo(Pokemon o) {
