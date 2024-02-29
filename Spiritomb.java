4d8a9904fde0172da072374da697337b27daa859
Michael Aurilia <135925083+Michael-Aurilia@users.noreply.github.com>
2024-02-28 22:39:54 -0500

    Update Spiritomb.java

--- Spiritomb.java
+++ Spiritomb.java
@@ -17,8 +17,7 @@ public class Spiritomb extends Pokemon{
 	
 	//Performs the attack on the opposing Pokemon based on the index.
 	@Override
-	public void attack(Pokemon other, int attackIndex) {
-		String battleCry8, PokemonInfo8;
+	public void attack(Pokemon myPokemon, Pokemon other, int attackIndex) {
 		List<String> moves8 = new ArrayList<>();
 		List<String> attackTypes8 = new ArrayList<>();
 		List<Integer> basePowers8 = new ArrayList<>();
@@ -28,33 +27,27 @@ public class Spiritomb extends Pokemon{
 				moves8.add("Shadow Ball");
 				attackTypes8.add("Ghost");
 				basePowers8.add(80);
-				moveDescriptions8.add("Ghost Type, Special: Spiritomb hurls a shadowy blob at the opposing Pokemon!");
+				moveDescriptions8.add("Ghost Type, Special: Spiritomb used Shadow Ball!");
 				Attack ShadowBall = new Attack(moveDescriptions8.get(0), 1, 15, attackTypes8.get(0), basePowers8.get(0), "Special");
 				
 				moves8.add("Dark Pulse");
 				attackTypes8.add("Dark");
 				basePowers8.add(80);
-				moveDescriptions8.add("Dark Type, Special: Spiritomb releases a horrible aura imbued with dark thoughts!");
+				moveDescriptions8.add("Dark Type, Special: Spiritomb used Dark Pulse!");
 				Attack DarkPulse = new Attack(moveDescriptions8.get(1), 1, 15, attackTypes8.get(1), basePowers8.get(1), "Special");
 				
 				//Will become Will-o-wisp
 				moves8.add("Icy Wind");
 				attackTypes8.add("Ice");
 				basePowers8.add(55);
-				moveDescriptions8.add("Ice Type, Special: Spiritomb attacks with a gust of chilled air!");
+				moveDescriptions8.add("Ice Type, Special: Spiritomb used Icy Wind!");
 				Attack IcyWind = new Attack(moveDescriptions8.get(2), 1, 15, attackTypes8.get(2), basePowers8.get(2), "Special");
 				
 				moves8.add("Hex");
 				attackTypes8.add("Ghost");
 				basePowers8.add(65);
-				moveDescriptions8.add("Ghost type, Special: Spiritomb attacks with relentless intensity!");
+				moveDescriptions8.add("Ghost type, Special: Spiritomb used Hex!");
 				Attack Hex = new Attack(moveDescriptions8.get(3), 1, 10, attackTypes8.get(3), basePowers8.get(3), "Special");
-				
-				PokemonInfo8 = "Spiritomb was formed by 108 spirits. It is bound to a fissure in an Odd Keystone.";
-				battleCry8 = "OOOOOO!";
-				
-				//HP, Type1, Type2, moves, battlecry, atk, def, spAtk, spDef, spe, Info
-				Spiritomb Tomb = new Spiritomb(157, 157, "Healthy", "Spiritomb", "Ghost", "Dark", moves8, battleCry8, 158, 176, 158, 176, 95, PokemonInfo8);
 				
 				int damageDealt;
 				int remainingHP;
@@ -62,7 +55,7 @@ public class Spiritomb extends Pokemon{
 				if (attackIndex == 1) {
 					System.out.println(moveDescriptions8.get(0));
 					//This could be the key to the computer calculating damage before the turn starts. Check if this works in the runner maybe
-					damageDealt = ShadowBall.getDamage(ShadowBall, Tomb, other);
+					damageDealt = ShadowBall.getDamage(ShadowBall, myPokemon, other);
 					remainingHP = other.getHitPoints() - damageDealt;
 					other.setHitPoints(remainingHP);
 					if (ShadowBall.getDamageMultiplier(attackTypes8.get(0), other) >= 2) {
@@ -78,7 +71,7 @@ public class Spiritomb extends Pokemon{
 				}
 				else if (attackIndex == 2) {
 					System.out.println(moveDescriptions8.get(1));
-					damageDealt = DarkPulse.getDamage(DarkPulse, Tomb, other);
+					damageDealt = DarkPulse.getDamage(DarkPulse, myPokemon, other);
 					remainingHP = other.getHitPoints() - damageDealt;
 					other.setHitPoints(remainingHP);
 					if (DarkPulse.getDamageMultiplier(attackTypes8.get(1), other) >= 2) {
@@ -94,7 +87,7 @@ public class Spiritomb extends Pokemon{
 				}
 				else if (attackIndex == 3) {
 					System.out.println(moveDescriptions8.get(2));
-					damageDealt = IcyWind.getDamage(IcyWind, Tomb, other);
+					damageDealt = IcyWind.getDamage(IcyWind, myPokemon, other);
 					remainingHP = other.getHitPoints() - damageDealt;
 					other.setHitPoints(remainingHP);
 					if (IcyWind.getDamageMultiplier(attackTypes8.get(2), other) >= 2) {
@@ -110,7 +103,7 @@ public class Spiritomb extends Pokemon{
 				}
 				else if (attackIndex == 4) {
 					System.out.println(moveDescriptions8.get(3));
-					damageDealt = Hex.getDamage(Hex, Tomb, other);
+					damageDealt = Hex.getDamage(Hex, myPokemon, other);
 					remainingHP = other.getHitPoints() - damageDealt;
 					other.setHitPoints(remainingHP);
 					if (Hex.getDamageMultiplier(attackTypes8.get(3), other) >= 2) {
@@ -127,7 +120,6 @@ public class Spiritomb extends Pokemon{
 	}
 	
 	public List<Integer> getDamages(Pokemon myPokemon, Pokemon other){
-		String battleCry8, PokemonInfo8;
 		List<String> moves8 = new ArrayList<>();
 		List<String> attackTypes8 = new ArrayList<>();
 		List<Integer> basePowers8 = new ArrayList<>();
@@ -137,38 +129,32 @@ public class Spiritomb extends Pokemon{
 				moves8.add("Shadow Ball");
 				attackTypes8.add("Ghost");
 				basePowers8.add(80);
-				moveDescriptions8.add("Ghost Type, Special: Spiritomb hurls a shadowy blob at the opposing Pokemon!");
+				moveDescriptions8.add("Ghost Type, Special: Spiritomb used Shadow Ball!");
 				Attack ShadowBall = new Attack(moveDescriptions8.get(0), 1, 15, attackTypes8.get(0), basePowers8.get(0), "Special");
 				
 				moves8.add("Dark Pulse");
 				attackTypes8.add("Dark");
 				basePowers8.add(80);
-				moveDescriptions8.add("Dark Type, Special: Spiritomb releases a horrible aura imbued with dark thoughts!");
+				moveDescriptions8.add("Dark Type, Special: Spiritomb used Dark Pulse!");
 				Attack DarkPulse = new Attack(moveDescriptions8.get(1), 1, 15, attackTypes8.get(1), basePowers8.get(1), "Special");
 				
 				//Will become Will-o-wisp
 				moves8.add("Icy Wind");
 				attackTypes8.add("Ice");
 				basePowers8.add(55);
-				moveDescriptions8.add("Ice Type, Special: Spiritomb attacks with a gust of chilled air!");
+				moveDescriptions8.add("Ice Type, Special: Spiritomb Used Icy Wind!");
 				Attack IcyWind = new Attack(moveDescriptions8.get(2), 1, 15, attackTypes8.get(2), basePowers8.get(2), "Special");
 				
 				moves8.add("Hex");
 				attackTypes8.add("Ghost");
 				basePowers8.add(65);
-				moveDescriptions8.add("Ghost type, Special: Spiritomb attacks with relentless intensity!");
+				moveDescriptions8.add("Ghost type, Special: Spiritomb used Hex!");
 				Attack Hex = new Attack(moveDescriptions8.get(3), 1, 10, attackTypes8.get(3), basePowers8.get(3), "Special");
 				
-				PokemonInfo8 = "Spiritomb was formed by 108 spirits. It is bound to a fissure in an Odd Keystone.";
-				battleCry8 = "OOOOOO!";
-				
-				//HP, Type1, Type2, moves, battlecry, atk, def, spAtk, spDef, spe, Info
-				Spiritomb Tomb = new Spiritomb(157, 157, "Healthy", "Spiritomb", "Ghost", "Dark", moves8, battleCry8, 158, 176, 158, 176, 95, PokemonInfo8);
-				
-				int damageDealt1 = ShadowBall.getDamage(ShadowBall, Tomb, other);
-				int damageDealt2 = DarkPulse.getDamage(DarkPulse, Tomb, other);
-				int damageDealt3 = IcyWind.getDamage(IcyWind, Tomb, other);
-				int damageDealt4 = Hex.getDamage(Hex, Tomb, other);
+				int damageDealt1 = ShadowBall.getDamage(ShadowBall, myPokemon, other);
+				int damageDealt2 = DarkPulse.getDamage(DarkPulse, myPokemon, other);
+				int damageDealt3 = IcyWind.getDamage(IcyWind, myPokemon, other);
+				int damageDealt4 = Hex.getDamage(Hex, myPokemon, other);
 				
 				List<Integer> damageList = new ArrayList<>();
 				damageList.add(damageDealt1);
