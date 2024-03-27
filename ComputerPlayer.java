ff084d4b790f6b93f5b990b63ab7f5baa6afdb42
Michael Aurilia <135925083+Michael-Aurilia@users.noreply.github.com>
2024-03-27 12:46:48 -0400

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -10,42 +10,58 @@ public class ComputerPlayer extends Player{
 	}
 	
 	public int computerChoice(Pokemon myPokemon, Pokemon other) {
-		//Add conditions for Pokemon Specific moves such as burn if target is healthy, use recover if low on hp, etc
+		int chosenAttack = 0;
 		
-		if (myPokemon.getMaxHitPoints() * 0.25 > myPokemon.getHitPoints() && getPotionCount() > 0) {
+		if (myPokemon.getAttacks().get(0).getmovePowerPoints() <= 0 && myPokemon.getAttacks().get(1).getmovePowerPoints() <= 0 
+	    		&& myPokemon.getAttacks().get(2).getmovePowerPoints() <= 0 && myPokemon.getAttacks().get(3).getmovePowerPoints() <= 0) {
+	    	return 6;
+	    }
+		
+		else if ("Milotic".equals(myPokemon.getName()) && myPokemon.getMaxHitPoints() * 0.25 > myPokemon.getHitPoints() && myPokemon.getAttacks().get(3).getmovePowerPoints() > 0) {
+		    chosenAttack = 3;
+		}
+
+		else if (myPokemon.getMaxHitPoints() * 0.25 > myPokemon.getHitPoints() && getPotionCount() > 0) {
+		    //Use a potion
 			return 1;
 		}
+
+		else if ("Spiritomb".equals(myPokemon.getName()) && "Healthy".equals(other.getStatus()) && !"Fire".equals(other.getType1()) && !"Fire".equals(other.getType2())
+				&& myPokemon.getAttacks().get(2).getmovePowerPoints() > 0) {
+			chosenAttack = 2;
+		}
 		
-		int highestDamageIndex = 0;
-		List<Integer> damageList = myPokemon.getDamages(myPokemon, other);
-		int max = damageList.get(0);
-		
-        for (int i = 1; i < damageList.size(); i++) {
-            if (damageList.get(i) > max) {
-                max = damageList.get(i);
-                highestDamageIndex = i;
-            }
-        }
+		else {
+			List<Integer> damageList = myPokemon.getDamages(myPokemon, other);
+			int max = damageList.get(0);
+	        for (int i = 1; i < damageList.size(); i++) {
+	        	if (damageList.get(i) > max) {
+	                max = damageList.get(i);
+	                chosenAttack = i;
+	            }
+	        }
+		}
 		
 		//Attack.
-		if (highestDamageIndex == 0) {
+		if (chosenAttack == 0) {
 			return 2;
 		}
-		else if (highestDamageIndex == 1) {
+		else if (chosenAttack == 1) {
 			return 3;
 		}
-		else if (highestDamageIndex == 2) {
+		else if (chosenAttack == 2) {
 			return 4;
 		}
-		else if (highestDamageIndex == 3) {
+		else if (chosenAttack == 3) {
 			return 5;
 		}
 		
 		//Base case that the computer shouldn't reach. Just prevents errors
-		return 2;
+		return 6;
 	}
 	
 	public void computerAction(int choice, Pokemon myPokemon, Pokemon other, Item potion) {
+		//Does the chosen action and decrements its usage by one
 		if (choice == 1) {
 			potion.useItem(myPokemon, myPokemon.getMaxHitPoints());
 			setPotionCount(getPotionCount() - 1);
@@ -53,21 +69,27 @@ public class ComputerPlayer extends Player{
 		
 		else if (choice == 2) {
 			myPokemon.attack(myPokemon, other, 1);
+			myPokemon.getAttacks().get(0).setmovePowerPoints(myPokemon.getAttacks().get(0).getmovePowerPoints() - 1);
 		}
 		
 		else if (choice == 3) {
 			myPokemon.attack(myPokemon, other, 2);
+			myPokemon.getAttacks().get(1).setmovePowerPoints(myPokemon.getAttacks().get(1).getmovePowerPoints() - 1);
 		}
 		
 		else if (choice == 4) {
 			myPokemon.attack(myPokemon, other, 3);
+			myPokemon.getAttacks().get(2).setmovePowerPoints(myPokemon.getAttacks().get(2).getmovePowerPoints() - 1);
 		}
 		
 		else if (choice == 5) {
 			myPokemon.attack(myPokemon, other, 4);
+			myPokemon.getAttacks().get(3).setmovePowerPoints(myPokemon.getAttacks().get(3).getmovePowerPoints() - 1);
+		}
+		else if (choice == 6) {
+			System.out.print(myPokemon.getName() + " has no moves left and can't attack!");
 		}
 	}
-
 	
 	//Mutators.
 	public Pokemon getMyPokemon() {
