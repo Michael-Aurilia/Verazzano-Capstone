79a201e11dc7268cec9c072ffe2b11097483677f
Michael Aurilia <135925083+Michael-Aurilia@users.noreply.github.com>
2024-03-27 12:47:27 -0400

    Update Garchomp.java

--- Garchomp.java
+++ Garchomp.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Garchomp extends Pokemon{
 	//Extra variable.
@@ -10,7 +11,7 @@ public class Garchomp extends Pokemon{
 	}
 	
 	//Takes info from the abstract Pokemon class to define Garchomp
-	public Garchomp(int hp, int mhp, String status, String name, String type1, String type2, List<String> attacks, String bc, double atk, double def, double spAtk, double spDef, double spe, String info) {
+	public Garchomp(int hp, int mhp, String status, String name, String type1, String type2, List<Attack> attacks, String bc, double atk, double def, double spAtk, double spDef, double spe, String info) {
 		super(hp, mhp, status, name, type1, type2, attacks, bc, atk, def, spAtk, spDef, spe);
 		setInfo(info);
 	}
@@ -18,149 +19,97 @@ public class Garchomp extends Pokemon{
 	//Performs the attack on the opposing Pokemon based on the index.
 	@Override
 	public void attack(Pokemon myPokemon, Pokemon other, int attackIndex) {
-		List<String> moves3 = new ArrayList<>();
-		List<String> attackTypes3 = new ArrayList<>();
-		List<Integer> basePowers3 = new ArrayList<>();
-		List<String> moveDescriptions3 = new ArrayList<>();
-	
-		//Creates attacks for this Pokemon to use.
-				moves3.add("Dragon Claw");
-				attackTypes3.add("Dragon");
-				basePowers3.add(80);
-				moveDescriptions3.add("Dragon Type, Physical: Garchomp used Dragon Claw!");
-				Attack DragonClaw = new Attack(moveDescriptions3.get(0), 1, 15, attackTypes3.get(0), basePowers3.get(0), "Physical");
+		int damageDealt;
+		int remainingHP;
 				
-				moves3.add("Fire Fang");
-				attackTypes3.add("Fire");
-				basePowers3.add(65);
-				moveDescriptions3.add("Fire Type, Physical: Garchomp used Fire Fang!");
-				Attack FireFang = new Attack(moveDescriptions3.get(1), 1, 15, attackTypes3.get(1), basePowers3.get(1), "Physical");
-				
-				moves3.add("Earthquake");
-				attackTypes3.add("Ground");
-				basePowers3.add(100);
-				moveDescriptions3.add("Ground Type, Physical: Garchomp used Earthquake!");
-				Attack Earthquake = new Attack(moveDescriptions3.get(2), 1, 10, attackTypes3.get(2), basePowers3.get(2), "Physical");
-				
-				moves3.add("Rock Slide");
-				attackTypes3.add("Rock");
-				basePowers3.add(75);
-				moveDescriptions3.add("Rock Type, Physical: Garchomp used Rock Slide!");
-				Attack RockSlide = new Attack(moveDescriptions3.get(3), 1, 15, attackTypes3.get(3), basePowers3.get(3), "Physical");
-				
-				int damageDealt;
-				int remainingHP;
-				
-				if (attackIndex == 1) {
-					System.out.println(moveDescriptions3.get(0));
-					//This could be the key to the computer calculating damage before the turn starts. Check if this works in the runner maybe
-					damageDealt = DragonClaw.getDamage(DragonClaw, myPokemon, other);
-					remainingHP = other.getHitPoints() - damageDealt;
-					other.setHitPoints(remainingHP);
-					if (DragonClaw.getDamageMultiplier(attackTypes3.get(0), other) >= 2) {
-						System.out.println("It's super effective!");
-					}
-					else if (DragonClaw.getDamageMultiplier(attackTypes3.get(0), other) == 0) {
-						System.out.println("The move had no effect.");
-					}
-					else if (DragonClaw.getDamageMultiplier(attackTypes3.get(0), other) <= 0.5) {
-						System.out.println("It's not very effective...");
-					}
-					System.out.println(other.getName() + " takes " + damageDealt + " damage!");
+		if (attackIndex == 1) {
+			System.out.println("Garchomp used Dragon Claw!");
+			damageDealt = myPokemon.getAttacks().get(0).getDamage(myPokemon.getAttacks().get(0), myPokemon, other);
+			remainingHP = other.getHitPoints() - damageDealt;
+			other.setHitPoints(remainingHP);
+			if (myPokemon.getAttacks().get(0).getDamageMultiplier(myPokemon.getAttacks().get(0).getAttackType(), other) >= 2) {
+				System.out.println("It's super effective!");
+			}
+			else if (myPokemon.getAttacks().get(0).getDamageMultiplier(myPokemon.getAttacks().get(0).getAttackType(), other) == 0) {
+				System.out.println("The move had no effect.");
+			}
+			else if (myPokemon.getAttacks().get(0).getDamageMultiplier(myPokemon.getAttacks().get(0).getAttackType(), other) <= 0.5) {
+				System.out.println("It's not very effective...");
+			}
+			System.out.println(other.getName() + " takes " + damageDealt + " damage!");
+		}
+		else if (attackIndex == 2) {
+			System.out.println("Garchomp used Fire Fang!");
+			damageDealt = myPokemon.getAttacks().get(1).getDamage(myPokemon.getAttacks().get(1), myPokemon, other);
+			remainingHP = other.getHitPoints() - damageDealt;
+			other.setHitPoints(remainingHP);
+			if (myPokemon.getAttacks().get(1).getDamageMultiplier(myPokemon.getAttacks().get(1).getAttackType(), other) >= 2) {
+				System.out.println("It's super effective!");
+			}
+			else if (myPokemon.getAttacks().get(1).getDamageMultiplier(myPokemon.getAttacks().get(1).getAttackType(), other) == 0) {
+				System.out.println("The move had no effect.");
+			}
+			else if (myPokemon.getAttacks().get(1).getDamageMultiplier(myPokemon.getAttacks().get(1).getAttackType(), other) <= 0.5) {
+				System.out.println("It's not very effective...");
+			}
+			System.out.println(other.getName() + " takes " + damageDealt + " damage!");
+			if (damageDealt > 0 && other.getHitPoints() > 0) {
+				Random random = new Random();
+				      int burnCheck = random.nextInt(10);
+				      if (burnCheck == 0 && other.getType1() != "Fire" && other.getType2() != "Fire") {
+					other.setStatus("Burned");
+					System.out.println(other.getName() + " was burned!");
+					other.setAttackStat(other.getAttackStat() / 2);
 				}
-				else if (attackIndex == 2) {
-					System.out.println(moveDescriptions3.get(1));
-					damageDealt = FireFang.getDamage(FireFang, myPokemon, other);
-					remainingHP = other.getHitPoints() - damageDealt;
-					other.setHitPoints(remainingHP);
-					if (FireFang.getDamageMultiplier(attackTypes3.get(1), other) >= 2) {
-						System.out.println("It's super effective!");
-					}
-					else if (FireFang.getDamageMultiplier(attackTypes3.get(1), other) == 0) {
-						System.out.println("The move had no effect.");
-					}
-					else if (FireFang.getDamageMultiplier(attackTypes3.get(1), other) <= 0.5) {
-						System.out.println("It's not very effective...");
-					}
-					System.out.println(other.getName() + " takes " + damageDealt + " damage!");
-				}
-				else if (attackIndex == 3) {
-					System.out.println(moveDescriptions3.get(2));
-					damageDealt = Earthquake.getDamage(Earthquake, myPokemon, other);
-					remainingHP = other.getHitPoints() - damageDealt;
-					other.setHitPoints(remainingHP);
-					if (Earthquake.getDamageMultiplier(attackTypes3.get(2), other) >= 2) {
-						System.out.println("It's super effective!");
-					}
-					else if (Earthquake.getDamageMultiplier(attackTypes3.get(2), other) == 0) {
-						System.out.println("The move had no effect.");
-					}
-					else if (Earthquake.getDamageMultiplier(attackTypes3.get(2), other) <= 0.5) {
-						System.out.println("It's not very effective...");
-					}
-					System.out.println(other.getName() + " takes " + damageDealt + " damage!");
-				}
-				else if (attackIndex == 4) {
-					System.out.println(moveDescriptions3.get(3));
-					damageDealt = RockSlide.getDamage(RockSlide, myPokemon, other);
-					remainingHP = other.getHitPoints() - damageDealt;
-					other.setHitPoints(remainingHP);
-					if (RockSlide.getDamageMultiplier(attackTypes3.get(3), other) >= 2) {
-						System.out.println("It's super effective!");
-					}
-					else if (RockSlide.getDamageMultiplier(attackTypes3.get(3), other) == 0) {
-						System.out.println("The move had no effect.");
-					}
-					else if (RockSlide.getDamageMultiplier(attackTypes3.get(3), other) <= 0.5) {
-						System.out.println("It's not very effective...");
-					}
-					System.out.println(other.getName() + " takes " + damageDealt + " damage!");
-				}
+			}
+		}
+		else if (attackIndex == 3) {
+			System.out.println("Garchomp used Earthquake!");
+			damageDealt = myPokemon.getAttacks().get(2).getDamage(myPokemon.getAttacks().get(2), myPokemon, other);
+			remainingHP = other.getHitPoints() - damageDealt;
+			other.setHitPoints(remainingHP);
+			if (myPokemon.getAttacks().get(2).getDamageMultiplier(myPokemon.getAttacks().get(2).getAttackType(), other) >= 2) {
+				System.out.println("It's super effective!");
+			}
+			else if (myPokemon.getAttacks().get(2).getDamageMultiplier(myPokemon.getAttacks().get(2).getAttackType(), other) == 0) {
+				System.out.println("The move had no effect.");
+			}
+			else if (myPokemon.getAttacks().get(2).getDamageMultiplier(myPokemon.getAttacks().get(2).getAttackType(), other) <= 0.5) {
+				System.out.println("It's not very effective...");
+			}
+			System.out.println(other.getName() + " takes " + damageDealt + " damage!");
+		}
+		else if (attackIndex == 4) {
+			System.out.println("Garchomp used Rock Slide");
+			damageDealt = myPokemon.getAttacks().get(3).getDamage(myPokemon.getAttacks().get(3), myPokemon, other);
+			remainingHP = other.getHitPoints() - damageDealt;
+			other.setHitPoints(remainingHP);
+			if (myPokemon.getAttacks().get(3).getDamageMultiplier(myPokemon.getAttacks().get(3).getAttackType(), other) >= 2) {
+				System.out.println("It's super effective!");
+			}
+			else if (myPokemon.getAttacks().get(3).getDamageMultiplier(myPokemon.getAttacks().get(3).getAttackType(), other) == 0) {
+				System.out.println("The move had no effect.");
+			}
+			else if (myPokemon.getAttacks().get(3).getDamageMultiplier(myPokemon.getAttacks().get(3).getAttackType(), other) <= 0.5) {
+				System.out.println("It's not very effective...");
+			}
+			System.out.println(other.getName() + " takes " + damageDealt + " damage!");
+		}
 	}
 	
-	public List<Integer> getDamages(Pokemon myPokemon, Pokemon other){
-		List<String> moves3 = new ArrayList<>();
-		List<String> attackTypes3 = new ArrayList<>();
-		List<Integer> basePowers3 = new ArrayList<>();
-		List<String> moveDescriptions3 = new ArrayList<>();
-	
-		//Creates attacks for this Pokemon to use.
-				moves3.add("Dragon Claw");
-				attackTypes3.add("Dragon");
-				basePowers3.add(80);
-				moveDescriptions3.add("Dragon Type, Physical: Garchomp slashes the foe with its huge sharp claws!");
-				Attack DragonClaw = new Attack(moveDescriptions3.get(0), 1, 15, attackTypes3.get(0), basePowers3.get(0), "Physical");
+	public List<Integer> getDamages(Pokemon myPokemon, Pokemon other) {
+		int damageDealt1 = myPokemon.getAttacks().get(0).getDamage(myPokemon.getAttacks().get(0), myPokemon, other);
+		int damageDealt2 = myPokemon.getAttacks().get(1).getDamage(myPokemon.getAttacks().get(1), myPokemon, other);
+		int damageDealt3 = myPokemon.getAttacks().get(2).getDamage(myPokemon.getAttacks().get(2), myPokemon, other);
+		int damageDealt4 = myPokemon.getAttacks().get(3).getDamage(myPokemon.getAttacks().get(3), myPokemon, other);
 				
-				moves3.add("Fire Fang");
-				attackTypes3.add("Fire");
-				basePowers3.add(65);
-				moveDescriptions3.add("Fire Type, Physical: Garchomp bites with flame-cloaked fangs!");
-				Attack FireFang = new Attack(moveDescriptions3.get(1), 1, 15, attackTypes3.get(1), basePowers3.get(1), "Physical");
+		List<Integer> damageList = new ArrayList<>();
+		damageList.add(damageDealt1);
+		damageList.add(damageDealt2);
+		damageList.add(damageDealt3);
+		damageList.add(damageDealt4);
 				
-				moves3.add("Earthquake");
-				attackTypes3.add("Ground");
-				basePowers3.add(100);
-				moveDescriptions3.add("Ground Type, Physical: Garchomp causes a massive earthquake to damage the opposing Pokemon!");
-				Attack Earthquake = new Attack(moveDescriptions3.get(2), 1, 10, attackTypes3.get(2), basePowers3.get(2), "Physical");
-				
-				moves3.add("Rock Slide");
-				attackTypes3.add("Rock");
-				basePowers3.add(75);
-				moveDescriptions3.add("Rock Type, Physical: Garchomp hurls Large boulders at the opposing Pokemon!");
-				Attack RockSlide = new Attack(moveDescriptions3.get(3), 1, 15, attackTypes3.get(3), basePowers3.get(3), "Physical");
-				
-				int damageDealt1 = DragonClaw.getDamage(DragonClaw, myPokemon, other);
-				int damageDealt2 = FireFang.getDamage(FireFang, myPokemon, other);
-				int damageDealt3 = Earthquake.getDamage(Earthquake, myPokemon, other);
-				int damageDealt4 = RockSlide.getDamage(RockSlide, myPokemon, other);
-				
-				List<Integer> damageList = new ArrayList<>();
-				damageList.add(damageDealt1);
-				damageList.add(damageDealt2);
-				damageList.add(damageDealt3);
-				damageList.add(damageDealt4);
-				
-				return damageList;
+		return damageList;
 	}
 
 	@Override
