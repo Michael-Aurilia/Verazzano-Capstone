57b0bfbf9af327e663a466ee16f29483e430989c
Michael Aurilia <135925083+Michael-Aurilia@users.noreply.github.com>
2024-04-15 19:05:10 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,5 +1,6 @@
+import java.util.List;
+
 //Interface that declares the use item function.
-//Add more than just potions by implementing this interface in some new files
 public interface Item{
-	public void useItem(Pokemon myPokemon, int maxHP, int count);
+	public void useItem(Pokemon myPokemon, List<Pokemon> team);
 }
